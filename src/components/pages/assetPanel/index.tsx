622b0d4fd6210353731bb8dd540c6f7efd4a33a8
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-13 10:34:05 +1100

    🎉 Update

--- src/components/pages/assetPanel/index.tsx
+++ src/components/pages/assetPanel/index.tsx
@@ -5,11 +5,7 @@ function AssetPanel(props: { onChangeAsset: (asset: string) => void; onChangeAmo
         <div>
             <select>
                 {approvedAssets.map((asset) => {
-                    return (
-                        <option className="focus:ring-indigo-500 focus:border-indigo-500 h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md">
-                            {asset.symbol}
-                        </option>
-                    );
+                    return <option className="h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-md">{asset.symbol}</option>;
                 })}
             </select>
         </div>
