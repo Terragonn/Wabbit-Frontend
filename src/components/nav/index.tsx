35003cc8d1a35e5e6b37618188c15206d71abe84
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-12 20:49:37 +1100

    🎉 Update

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -3,13 +3,17 @@ import NavLink, { Link } from "./navLink";
 
 function Nav(props: { navLinks: Link[]; current: number; setPageId: (pageId: number) => void }) {
     return (
-        <nav className="bg-zinc-900 mx-auto p-6 flex items-center justify-evenly">
-            <div className="mx-auto flex items-center justify-between">
-                {props.navLinks.map((link, index) => (
-                    <NavLink key={index} current={link.id === props.current} link={link} setPageId={props.setPageId} />
-                ))}
+        <nav className="bg-zinc-900 mx-auto p-6">
+            <div className="mx-auto w-5/6 flex items-center justify-evenly">
+                <div className="mx-auto flex items-center justify-evenly">
+                    {props.navLinks.map((link, index) => (
+                        <NavLink key={index} current={link.id === props.current} link={link} setPageId={props.setPageId} />
+                    ))}
+                </div>
+                <span className="w-5/12">
+                    <Wallet />
+                </span>
             </div>
-            <Wallet />
         </nav>
     );
 }
