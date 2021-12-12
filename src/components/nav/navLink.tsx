b9c4806da9ecf5469c7695327b8d2d28c6024df5
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-12 20:55:00 +1100

    🎉 Update

--- src/components/nav/navLink.tsx
+++ src/components/nav/navLink.tsx
@@ -5,12 +5,12 @@ export interface Link {
 
 function NavLink(props: { current: boolean; link: Link; setPageId: (pageId: number) => void }) {
     return (
-        <a
+        <button
             className={`cursor-pointer ${props.current ? "bg-zinc-700" : ""} py-3 px-6 mx-12 rounded-md text-white font-medium`}
             onClick={() => props.setPageId(props.link.id)}
         >
             {props.link.name}
-        </a>
+        </button>
     );
 }
 
