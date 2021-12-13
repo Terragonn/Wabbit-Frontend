aa4c3f32f17d80605d5b7bd0fd224befd3e75bc4
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-13 19:42:24 +1100

    🎉 Update

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -10,7 +10,7 @@ function Nav(props: { navLinks: Link[]; current: number; setPageId: (pageId: num
     return (
         <>
             {modalVisible ? <NavModal /> : null}
-            <nav className="bg-zinc-900 mx-auto p-6 text-center lg:drop-shadow-md drop-shadow-none relative z-20">
+            <nav className="bg-zinc-900 mx-auto p-6 text-center drop-shadow-md relative z-20">
                 <div className="mx-auto w-5/6 flex items-center justify-evenly">
                     <h1 className="mx-auto text-white font-bold text-lg">Wabbit</h1>
                     <div className="mx-auto items-center justify-evenly lg:flex hidden">
@@ -21,7 +21,7 @@ function Nav(props: { navLinks: Link[]; current: number; setPageId: (pageId: num
                     <span className="mx-auto w-5/12 lg:block hidden">
                         <Wallet />
                     </span>
-                    <div className="mx-auto cursor-pointer" onClick={() => setModalVisible(!modalVisible)}>
+                    <div className="mx-auto cursor-pointer lg:hidden block" onClick={() => setModalVisible(!modalVisible)}>
                         <div className="px-4 pt-1 my-1 bg-white" />
                         <div className="px-4 pt-1 my-1 bg-white" />
                         <div className="px-4 pt-1 my-1 bg-white" />
