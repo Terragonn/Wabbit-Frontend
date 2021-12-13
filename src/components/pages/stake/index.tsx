5d84e611cd09a93c393ee339be7fc04085460f9f
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-13 12:10:18 +1100

    🎉 Update

--- src/components/pages/stake/index.tsx
+++ src/components/pages/stake/index.tsx
@@ -1,9 +1,13 @@
 import Deposit from "./deposit";
+import Withdraw from "./withdraw";
 
 function Stake() {
     return (
         <div>
+            {/* **** Also add in whether or not the period epilogue is present */}
             <Deposit />
+            <div className="my-16" />
+            <Withdraw />
         </div>
     );
 }
