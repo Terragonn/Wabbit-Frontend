ae11cb30a4d2d4f4647b67c12a9432ac70eeb492
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-13 21:02:49 +1100

    🎉 Update

--- src/components/pages/borrow/index.tsx
+++ src/components/pages/borrow/index.tsx
@@ -1,9 +1,20 @@
+import Borrow from "./borrow";
+import Deposit from "./deposit";
+import Repay from "./repay";
+
 function BorrowPage(props: {}) {
     return (
         <div className="flex items-stretch justify-between lg:flex-row flex-col">
             {/* **** Do some sort of grid layout with aside */}
-            <div className="w-full lg:mx-5 lg:my-0 mb-10"></div>
-            <div className="w-full lg:mx-5"></div>
+            <div className="w-full lg:mx-5 lg:my-0 mb-10">
+                <Borrow />
+            </div>
+            <div className="w-full lg:mx-5">
+                <Deposit />
+            </div>
+            <div className="w-full lg:mx-5">
+                <Repay />
+            </div>
         </div>
     );
 }
