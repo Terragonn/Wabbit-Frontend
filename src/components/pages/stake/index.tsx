631bf76e7a2aefab7a3f347c48ce3fc0b1d5501b
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-13 16:49:21 +1100

    🎉 Update

--- src/components/pages/stake/index.tsx
+++ src/components/pages/stake/index.tsx
@@ -3,13 +3,13 @@ import Withdraw from "./withdraw";
 
 function Stake() {
     return (
-        <div className="flex items-stretch justify-between">
+        <div className="flex items-stretch justify-between lg:flex-row flex-col">
             {/* **** Also add in whether or not the period epilogue is present */}
-            <div className="w-full mx-5">
+            <div className="w-full lg:mx-5 lg:my-0 mb-10">
                 <Deposit />
             </div>
-            <div className="p-0.25 border-2 border-zinc-800" />
-            <div className="w-full mx-5">
+            <div className="p-0.25 border-2 border-zinc-800 lg:visible invisible" />
+            <div className="w-full lg:mx-5">
                 <Withdraw />
             </div>
         </div>
