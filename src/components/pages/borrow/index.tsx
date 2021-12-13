cdc46cd274abeb70e3e6d24f8bb0dfdaab3c612e
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-13 21:49:32 +1100

    🎉 Update

--- src/components/pages/borrow/index.tsx
+++ src/components/pages/borrow/index.tsx
@@ -4,6 +4,8 @@ import Repay from "./repay";
 import Withdraw from "./withdraw";
 
 function BorrowPage(props: {}) {
+    // **** Dont forget the props for the actual implementation
+
     return (
         <div className="grid lg:grid-cols-2 grid-cols-1 gap-6 mx-5 text-base text-white mb-4">
             <Deposit />
