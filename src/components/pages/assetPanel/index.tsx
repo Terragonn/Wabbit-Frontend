0919a07a8a8429c55f4e3d1f09ea7f6839541c65
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-13 11:08:00 +1100

    🎉 Update

--- src/components/pages/assetPanel/index.tsx
+++ src/components/pages/assetPanel/index.tsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import approvedAssets from "../../../approved.json";
 import { AssetData } from "../home/row";
 
 function AssetPanel(props: { onChangeAsset: (asset: AssetData) => void; onChangeAmount: (amount: number) => void }) {
     const [asset, setCurrentAsset] = useState<AssetData>(approvedAssets[0]);
+    const [amount, setAmount] = useState<number>(0);
+
+    useEffect(() => {
+        props.onChangeAsset(asset);
+    }, [asset]);
+
+    useEffect(() => {
+        props.onChangeAmount(amount);
+    }, [amount]);
 
     return (
-        <div className="flex items-center justify-evenly text-gray-500 text-lg font-medium">
+        <div className="flex items-center justify-evenly text-gray-500 text-lg font-medium p-4">
             <img src={asset.icon} alt={asset.symbol} width={32} className="mr-4 rounded-md" />
             <select
                 className="py-1 pl-2 pr-7 border-transparent bg-transparent rounded-md w-5/12"
                 onChange={(e) => {
                     const asset = approvedAssets.filter((_asset) => _asset.address === e.target.value)[0];
-                    props.onChangeAsset(asset);
                     setCurrentAsset(asset);
                 }}
             >
@@ -23,10 +31,11 @@ function AssetPanel(props: { onChangeAsset: (asset: AssetData) => void; onChange
             <input
                 type="number"
                 min={0}
-                placeholder="0.00"
+                value={amount}
                 step="0.01"
+                placeholder="0.00"
                 className="w-full text-center bg-transparent border-transparent rounded-md"
-                onChange={(e) => props.onChangeAmount(e.target.valueAsNumber)}
+                onChange={(e) => setAmount(e.target.valueAsNumber)}
             />
         </div>
     );
