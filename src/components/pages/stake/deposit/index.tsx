faa484830c973a755bfddef90f3deb9b02830a30
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-12-14 21:49:16 +1100

    🎉 Update

--- src/components/pages/stake/deposit/index.tsx
+++ src/components/pages/stake/deposit/index.tsx
@@ -3,6 +3,9 @@ import AssetPanel from "../../../assetPanel";
 import { AssetData } from "../../home/row";
 import config from "../../../../config/config.json";
 import useContracts from "../../../../utils/useContracts";
+import parseBigNumber from "../../../../utils/parseBigNumber";
+import { ethers } from "ethers";
+import parseNumber from "../../../../utils/parseNumber";
 
 interface Data {
     available: string;
@@ -20,10 +23,33 @@ function Deposit(props: {}) {
     const [data, setData] = useState<Data | null>(null);
 
     useEffect(() => {
+        // Get the contracts
+        const pool = contracts?.pool;
+        const oracle = contracts?.oracle;
+        const margin = contracts?.margin;
+
+        // Get the data
         (async () => {
             const tempData: Data = {} as any;
+
+            tempData.available = parseBigNumber(await margin?.liquidityAvailable(asset.address, pool?.address), asset.decimals);
+            tempData.borrowed = parseBigNumber(await margin?.totalBorrowed(asset.address, pool?.address), asset.decimals);
+            tempData.tvl = parseBigNumber(await pool?.getLiquidity(asset.address), asset.decimals);
+
+            if (parseInt(tempData.tvl) > 0) {
+                const decimals = await oracle?.getDecimals();
+                const periodLength = await pool?.getPeriodLength();
+                const interestRate = await margin?.calculateInterestRate(asset.address, pool?.address);
+
+                const periodsPerYear = ethers.BigNumber.from(3.154e7).div(periodLength);
+                const apy = interestRate.mul(periodsPerYear);
+
+                tempData.apy = parseBigNumber(apy, decimals.div(100).toNumber());
+            } else tempData.apy = parseNumber(0);
+
+            setData(tempData);
         })();
-    }, []);
+    }, [contracts, asset]);
 
     return (
         <div className="flex flex-col justify-center items-stretch">
@@ -31,10 +57,10 @@ function Deposit(props: {}) {
             <AssetPanel onChangeAsset={setAsset} onChangeAmount={setAmount} />
             {/* Update these with the actual values */}
             <div className="grid grid-cols-2 gap-6 mx-5 text-base text-white mb-4">
-                <h2>Liquidity: 2.6B</h2>
-                <h2>Borrowed: 1.4B</h2>
-                <h2>TVL: 4.0B</h2>
-                <h2>APY: 23.05%</h2>
+                <h2>Liquidity: {data?.available}</h2>
+                <h2>Borrowed: {data?.borrowed}</h2>
+                <h2>TVL: {data?.tvl}</h2>
+                <h2>APY: {data?.apy}%</h2>
             </div>
             <button className="bg-indigo-600 hover:bg-indigo-700 p-3 rounded-md text-white font-medium">
                 Deposit {amount} {asset.symbol}
