be095416f8f971704a84504fee62a2a4c54d8c04
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-03 10:18:20 +1100

    🎉 Update

--- src/pages/dashboard/tableHeader.tsx
+++ src/pages/dashboard/tableHeader.tsx
@@ -3,7 +3,8 @@ export default function TableHeader() {
 
     return (
         <div className="flex items-center justify-evenly py-3 px-10 mb-2">
-            {headers.map((header) => (
+            <h3 className="text-left w-1/5 text-neutral-500 font-bold">{headers[0]}</h3>
+            {headers.slice(1).map((header) => (
                 <h3 className="text-center w-1/5 text-neutral-500 font-bold">{header}</h3>
             ))}
         </div>
--- src/pages/dashboard/tableRow.tsx
+++ src/pages/dashboard/tableRow.tsx
@@ -17,8 +17,8 @@ export default function TableRow({
 }) {
     return (
         <div className="bg-neutral-900 rounded-xl flex items-center justify-evenly py-9 px-10 text-center font-bold text-white text-xl mb-8 shadow-md shadow-fuchsia-500">
-            <span className="flex items-center justify-evenly w-1/5 space-x-1">
-                <img src={icon} width={34} className="rounded-xl" />
+            <span className="flex items-center justify-start w-1/5 space-x-3">
+                <img src={icon} width={40} className="rounded-xl" />
                 <span>{name}</span>
                 <span className="text-neutral-500">({symbol})</span>
             </span>
