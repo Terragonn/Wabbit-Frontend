87883f4812685e95850a6cf2e50a993f799ecca4
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-03 10:35:03 +1100

    🎉 Update

--- src/pages/dashboard/tableRow.tsx
+++ src/pages/dashboard/tableRow.tsx
@@ -5,6 +5,7 @@ export default function TableRow({
     tvl,
     borrowed,
     stakeAPY,
+    yieldAPR,
     borrowAPY,
 }: {
     name: string;
@@ -13,18 +14,26 @@ export default function TableRow({
     tvl: string;
     borrowed: string;
     stakeAPY: string;
+    yieldAPR: string;
     borrowAPY: string;
 }) {
     return (
         <div className="bg-neutral-900 rounded-xl flex items-center justify-evenly py-9 px-10 text-center font-bold text-white text-xl mb-8 shadow-lg shadow-fuchsia-500/20">
-            <span className="flex items-center justify-start w-1/5 space-x-3">
+            <span className="flex items-center justify-start w-1/5 space-x-3 font-medium">
                 <img src={icon} width={40} className="rounded-xl" />
                 <span>{name}</span>
                 <span className="text-neutral-500">({symbol})</span>
             </span>
             <span className="w-1/5">$ {tvl}</span>
             <span className="w-1/5">$ {borrowed}</span>
-            <span className="w-1/5">{stakeAPY} %</span>
+            <div className="w-1/5">
+                <span>{stakeAPY} %</span>
+                <span className="flex items-center justify-evenly mt-2 w-3/5 mx-auto text-lg">
+                    <img src={require("../../images/TOKEN.png")} width={27} />
+                    <span>{yieldAPR} %</span>
+                    <span className="text-neutral-400">APR</span>
+                </span>
+            </div>
             <span className="w-1/5">{borrowAPY} %</span>
         </div>
     );
