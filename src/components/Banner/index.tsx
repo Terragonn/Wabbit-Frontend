0049bc15b52b56be7dfb7958f73c1908b0abf5c1
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-03 08:15:50 +1100

    🎉 Update

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -5,10 +5,11 @@ interface Placeholder {
 
 export default function Banner({ placeholders }: { placeholders: Placeholder[] }) {
     return (
-        <div>
+        <div className="bg-neutral-900 px-16 py-8 flex items-center justify-evenly rounded-xl shadow-md shadow-fuchsia-500 my-16">
             {placeholders.map((placeholder) => (
-                <div>
-                    <h2>{placeholders}</h2>
+                <div className="text-center">
+                    <h2 className="text-neutral-500 font-bold text-xl">{placeholder.title}</h2>
+                    <p className="text-white font-bold text-3xl">{placeholder.body}</p>
                 </div>
             ))}
         </div>
--- src/pages/dashboard/leverage/index.tsx
+++ src/pages/dashboard/leverage/index.tsx
@@ -1,5 +1,9 @@
 import Banner from "../../../components/Banner";
 
 export default function DashboardLeverage() {
-    return <div>Hello World</div>;
+    return (
+        <div className="w-5/6 mx-auto">
+            <Banner placeholders={[{ title: "Total Value Locked", body: "$ 138,245,234" }]} />
+        </div>
+    );
 }
