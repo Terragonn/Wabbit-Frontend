ac60bc3e1aa34ac2b53ec2d7f849e22f35f98cc4
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-03 20:19:58 +1100

    🎉 Update

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -4,9 +4,9 @@ export default function Nav({ setNavState }: { setNavState: (state: boolean) => 
     return (
         <nav className="py-8 flex items-center xl:justify-end justify-between">
             <div className="xl:hidden block cursor-pointer" onClick={(e) => setNavState(true)}>
-                <div className="px-5 py-1 my-1 bg-white rounded-md" />
-                <div className="px-5 py-1 my-1 bg-white rounded-md" />
-                <div className="px-5 py-1 my-1 bg-white rounded-md" />
+                <div className="px-5 py-1 my-1 bg-white glow rounded-md" />
+                <div className="px-5 py-1 my-1 bg-white glow rounded-md" />
+                <div className="px-5 py-1 my-1 bg-white glow rounded-md" />
             </div>
             <Wallet />
         </nav>
