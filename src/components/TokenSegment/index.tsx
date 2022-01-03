b3398818f462a03d2711f058abfb114d312be356
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-03 13:48:24 +1100

    🎉 Update

--- src/components/TokenSegment/index.tsx
+++ src/components/TokenSegment/index.tsx
@@ -5,14 +5,16 @@ export default function TokenSegment({ title, keys, cta }: { title: string; keys
             <div className="bg-neutral-900 rounded-3xl p-3 glow w-full text-center">
                 <input className="bg-transparent border-none rounded-xl text-center text-white text-xl font-bold" type="number" value="0.00" min={0} step={0.01} />
             </div>
-            <div className="mt-16 w-4/5 mx-auto">
-                {Object.entries(keys).map(([key, value], index) => (
-                    <div className="flex items-center justify-between text-white font-bold text-lg mb-5">
-                        <span>{key}:</span>
-                        <span>{value}</span>
-                    </div>
-                ))}
-                <button className="bg-fuchsia-700 glow text-white font-bold text-3xl px-5 py-2.5 rounded-xl">{cta}</button>
+            <div className="mt-16 w-4/5 mx-auto flex flex-col items-stretch justify-evenly">
+                <div>
+                    {Object.entries(keys).map(([key, value], index) => (
+                        <div className="flex items-center justify-between text-white font-bold text-lg mb-6">
+                            <span>{key}:</span>
+                            <span>{value}</span>
+                        </div>
+                    ))}
+                </div>
+                <button className="bg-fuchsia-700 glow text-white font-bold text-3xl px-5 py-2.5 rounded-xl mt-5 hover:bg-fuchsia-600">{cta}</button>
             </div>
         </>
     );
