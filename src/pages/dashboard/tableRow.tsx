b7ee1c18eb99fb239bf864fb8f0b2423ac2fba8c
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-03 14:27:03 +1100

    🎉 Update

--- src/pages/dashboard/tableRow.tsx
+++ src/pages/dashboard/tableRow.tsx
@@ -19,18 +19,20 @@ export default function TableRow({
 }) {
     return (
         <div className="bg-neutral-900 rounded-xl flex items-center justify-evenly py-9 px-10 text-center font-bold text-white text-2xl mb-10 glow">
-            <span className="flex items-center justify-start w-1/5 space-x-3 font-medium text-xl">
+            <span className="flex items-center justify-start w-1/5 space-x-5 font-medium text-xl">
                 <img src={icon} width={40} className="rounded-xl" alt={name} />
-                <span>{name}</span>
-                <span className="text-neutral-500">({symbol})</span>
+                <div className="flex flex-col items-center justify-evenly">
+                    <span>{name}</span>
+                    <span className="text-neutral-500">({symbol})</span>
+                </div>
             </span>
             <span className="w-1/5">$ {tvl}</span>
             <span className="w-1/5">$ {borrowed}</span>
             <div className="w-1/5">
                 <span>{stakeAPY} %</span>
-                <span className="flex items-center justify-evenly mt-2 w-3/5 mx-auto text-lg">
+                <span className="flex items-center justify-evenly mt-2 w-3/5 mx-auto text-lg space-x-2">
                     <img src={require("../../images/TOKEN.png")} width={28} alt="Torque TAU" />
-                    <span>{yieldAPR} %</span>
+                    <span className="whitespace-nowrap">{yieldAPR} %</span>
                     <span className="text-neutral-400">APR</span>
                 </span>
             </div>
