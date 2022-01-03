e2f644e68fe84b66df32c3a3c6c5947eca32343c
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-03 11:20:07 +1100

    🎉 Update

--- src/components/wallet/index.tsx
+++ src/components/wallet/index.tsx
@@ -1,3 +1,3 @@
 export default function Wallet() {
-    return <button className="bg-neutral-900 px-6 py-3 rounded-xl text-white font-bold text-2xl hover:bg-fuchsia-700 shadow-lg shadow-fuchsia-500/20">Connect</button>;
+    return <button className="bg-neutral-900 px-12 py-6 rounded-xl text-white font-bold text-3xl hover:bg-fuchsia-700 shadow-lg shadow-fuchsia-500/20">Connect</button>;
 }
