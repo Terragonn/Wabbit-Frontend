ef3ebeb24cc5695a57c69cae5ad1b9615dc3a67e
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-03 21:47:41 +1100

    🎉 Update

--- src/pages/leverage/index.tsx
+++ src/pages/leverage/index.tsx
@@ -15,7 +15,7 @@ export default function Leverage() {
                 />
             </div>
             <div className="p-12 bg-neutral-900 rounded-xl glow flex flex-col items-start justify-evenly pb-10 lg:my-0 my-20">
-                <div className="w-full mb-10 lg:mb-0 mb-20">
+                <div className="w-full lg:mb-10 mb-20">
                     <TokenSelect title="Token" />
                 </div>
                 <div className="flex lg:items-start items-stretch justify-between lg:space-y-0 space-y-20 lg:flex-row flex-col w-full">
@@ -25,7 +25,7 @@ export default function Leverage() {
                     <div className="w-full lg:mx-5">
                         <TokenSegment title="Withdraw" keys={{ "Redeem amount": "25.36 tlDAI", "Redeem value": "$ 25.36" }} cta="Withdraw" />
                     </div>
-                    <div className="w-full flex flex-col items-center justify-center">
+                    <div className="w-full flex flex-col lg:items-center items-stretch justify-center">
                         <TokenSegment title="Leverage" keys={{ "Redeem amount": "25.36 tlDAI", "Redeem value": "$ 25.36" }} cta="Leverage" />
                         <button className="bg-fuchsia-700 glow text-white font-bold text-3xl px-5 py-2.5 rounded-xl mt-5 hover:bg-fuchsia-600 lg:w-4/5 w-full">
                             Repay
