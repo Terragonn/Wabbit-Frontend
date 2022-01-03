f478e06e31aef17fcad8af2f6d2f8be82ae30003
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-03 21:44:42 +1100

    🎉 Update

--- src/pages/leverage/index.tsx
+++ src/pages/leverage/index.tsx
@@ -15,10 +15,10 @@ export default function Leverage() {
                 />
             </div>
             <div className="p-12 bg-neutral-900 rounded-xl glow flex flex-col items-start justify-evenly pb-10 lg:my-0 my-20">
-                <div className="w-full mb-10">
+                <div className="w-full mb-10 lg:mb-0 mb-20">
                     <TokenSelect title="Token" />
                 </div>
-                <div className="flex items-start justify-between">
+                <div className="flex lg:items-start items-stretch justify-between lg:space-y-0 space-y-20 lg:flex-row flex-col w-full">
                     <div className="w-full">
                         <TokenSegment title="Deposit" keys={{ "Staked amount": "25.36 DAI", "Staked value": "$ 25.36" }} cta="Deposit" />
                     </div>
