815309477bd2db82884d277a28cda13167a689b5
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-03 22:19:55 +1100

    🎉 Update

--- src/pages/yield/index.tsx
+++ src/pages/yield/index.tsx
@@ -8,28 +8,22 @@ export default function Yield() {
             <div className="lg:block hidden">
                 <Banner
                     placeholders={[
-                        { title: "Yield APR", body: "22.36 %" },
-                        { title: "Total Staked", body: "1,161,139 DAI" },
+                        { title: "Stake APY", body: "25.36 %" },
+                        { title: "Total Locked", body: "2,361,132 DAI" },
+                        { title: "Total Value Locked", body: "$ 138,245,234" },
                     ]}
                 />
             </div>
-            <div className="p-12 bg-neutral-900 rounded-xl glow flex flex-col items-start justify-evenly pb-10 lg:my-0 my-20">
-                <div className="w-full lg:mb-16 mb-20">
+            <div className="p-12 bg-neutral-900 rounded-xl glow flex items-start justify-evenly lg:space-y-0 space-y-20 pb-10 lg:flex-row flex-col lg:my-0 my-20">
+                <div className="lg:w-1/5 w-full">
                     <TokenSelect title="Token" />
                 </div>
-                <div className="flex lg:items-start items-stretch justify-between lg:space-y-0 space-y-20 lg:flex-row flex-col w-full">
-                    <div className="w-full">
-                        <TokenSegment title="Deposit" keys={{ "Staked amount": "25.36 DAI", "Staked value": "$ 25.36" }} cta="Deposit" />
-                    </div>
-                    <div className="w-full lg:mx-8">
-                        <TokenSegment title="Withdraw" keys={{ "Redeem amount": "25.36 tlDAI", "Redeem value": "$ 25.36" }} cta="Withdraw" />
-                    </div>
-                    <div className="w-full flex flex-col lg:items-center items-stretch justify-center">
-                        <TokenSegment title="Leverage" keys={{ "Redeem amount": "25.36 tlDAI", "Redeem value": "$ 25.36" }} cta="Leverage" />
-                        <button className="bg-fuchsia-700 glow text-white font-bold text-3xl px-5 py-2.5 rounded-xl mt-5 hover:bg-fuchsia-600 lg:w-4/5 w-full">
-                            Repay
-                        </button>
-                    </div>
+                <div className="lg:w-2/5 w-full lg:mx-10">
+                    <TokenSegment title="Stake" keys={{ "Staked amount": "25.36 DAI", "Staked value": "$ 25.36" }} cta="Deposit" />
+                </div>
+                <div className="lg:w-2/5 w-full flex flex-col lg:items-center items-stretch justify-center">
+                    <TokenSegment title="Withdraw" keys={{ "Redeem amount": "25.36 tlDAI", "Redeem value": "$ 25.36" }} cta="Withdraw" />
+                    <button className="bg-fuchsia-700 glow text-white font-bold text-3xl px-5 py-2.5 rounded-xl mt-5 hover:bg-fuchsia-600 lg:w-4/5 w-full">Claim</button>
                 </div>
             </div>
         </div>
