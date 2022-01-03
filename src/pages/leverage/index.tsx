f63f332718b2e07ab450d0615db0f5757605aa55
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-03 22:52:46 +1100

    🎉 Update

--- src/pages/leverage/index.tsx
+++ src/pages/leverage/index.tsx
@@ -21,13 +21,13 @@ export default function Leverage() {
                 </div>
                 <div className="flex lg:items-start items-stretch justify-between lg:space-y-0 space-y-20 lg:flex-row flex-col w-full">
                     <div className="w-full">
-                        <TokenSegment title="Deposit" keys={{ "Staked amount": "25.36 DAI", "Staked value": "$ 25.36" }} cta="Deposit" />
+                        <TokenSegment title="Deposit" keys={{ Collateral: "25.36 DAI", "Collateral value": "$ 25.36" }} cta="Deposit" />
                     </div>
                     <div className="w-full lg:mx-8">
-                        <TokenSegment title="Withdraw" keys={{ "Redeem amount": "25.36 tlDAI", "Redeem value": "$ 25.36" }} cta="Withdraw" />
+                        <TokenSegment title="Withdraw" keys={{ Maximum: "25.36 tlDAI", "Min margin level": "$ 25.36" }} cta="Withdraw" />
                     </div>
                     <div className="w-full flex flex-col lg:items-center items-stretch justify-center">
-                        <TokenSegment title="Leverage" keys={{ "Redeem amount": "25.36 tlDAI", "Redeem value": "$ 25.36" }} cta="Leverage" />
+                        <TokenSegment title="Leverage" keys={{ "Margin level": "1.35", "Margin balance": "$ 25.36", Borrowed: "27.45 DAI" }} cta="Leverage" />
                         <button className="bg-fuchsia-700 glow text-white font-bold text-3xl px-5 py-2.5 rounded-xl mt-5 hover:bg-fuchsia-600 lg:w-4/5 w-full">
                             Repay
                         </button>
