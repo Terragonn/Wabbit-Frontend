5b3d99ec4728deb22a085d4ff0c4c40c8620dd74
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-03 11:29:05 +1100

    🎉 Update

--- src/pages/stake/leverage/index.tsx
+++ src/pages/stake/leverage/index.tsx
@@ -1,3 +1,15 @@
+import Banner from "../../../components/Banner";
+
 export default function StakeLeverage() {
-    return <div></div>;
+    return (
+        <div>
+            <Banner
+                placeholders={[
+                    { title: "Stake APY", body: "25.36 %" },
+                    { title: "Total Locked", body: "2,361,132 DAI" },
+                    { title: "Total Value Locked", body: "$ 138,245,234" },
+                ]}
+            />
+        </div>
+    );
 }
