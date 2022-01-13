a27de5f0ac531b47ae604c08e5627ba248697529
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-14 10:27:49 +1100

    🎉 Update

--- src/utils/getApproved.ts
+++ src/utils/getApproved.ts
@@ -1,5 +1,20 @@
 import config from "../config/config.json";
 
+export interface Approved {
+    name: string;
+    symbol: string;
+    icon: string;
+    address: string;
+    whale: string;
+    decimals: number;
+    priceFeed: string;
+    reservePriceFeed: string;
+    marginLongCollateral: boolean;
+    marginLongBorrow: boolean;
+    leveragePool: boolean;
+    oracle: boolean;
+}
+
 export default function getApproved(address: string) {
     const item = config.approved.filter((approved) => approved.address === address);
     if (item.length === 0) return undefined;
