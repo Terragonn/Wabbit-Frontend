6180eeaa867af9aced07e5c4e2cf7f55bdb51837
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-18 14:18:25 +1100

    🎉 Update

--- src/components/AgreementModal/index.tsx
+++ src/components/AgreementModal/index.tsx
@@ -15,12 +15,17 @@ export default function AgreementModal() {
         <div className={`${agreed ? "hidden" : ""} bg-black bg-opacity-80 fixed inset-0 flex items-center justify-center z-50 modal`}>
             <div className="mx-auto lg:w-2/5 w-4/5 min-w-min bg-neutral-900 glow rounded-xl p-6">
                 <h2 className="text-white font-bold text-xl mb-2">Disclaimer Agreement</h2>
-                <p className="text-neutral-400 font-medium mb-3">
-                    By using this app you acknowledge the risks of cryptocurrencies, tokens, smart contracts, and leveraged trading.
-                </p>
-                <p className="text-neutral-400 font-medium pb-5 border-b-2 border-b-neutral-800 border-opacity-30">
-                    You acknowledge that using this app is risky and that you are responsible for ALL losses incurred on the platform.
-                </p>
+                <div className="text-neutral-400 font-medium border-b-2 pb-5 border-b-neutral-800 border-opacity-30 space-y-3">
+                    <p>By using this app you acknowledge the risks of cryptocurrencies, tokens, smart contracts, and leveraged trading.</p>
+                    <p>You acknowledge that using this app is risky and that you are responsible for ALL losses incurred on the platform.</p>
+                    <p>
+                        You have read through the{" "}
+                        <a href="https://docs.torque.money" className="font-bold text-neutral-300 hover:text-neutral-200">
+                            documentation
+                        </a>{" "}
+                        thoroughly and are confident in your understanding of how the protocol functions.
+                    </p>
+                </div>
 
                 <div className="flex lg:items-center items-stretch lg:space-y-0 space-y-3 justify-between mt-5 lg:flex-row flex-col">
                     <label className="flex items-center justify-start cursor-pointer">
