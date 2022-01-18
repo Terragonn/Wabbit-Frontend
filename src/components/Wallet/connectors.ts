1a780aee3825b1afbf70a4a7bc27d527a06ca2a1
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-18 21:53:41 +1100

    🎉 Update

--- src/components/Wallet/connectors.ts
+++ src/components/Wallet/connectors.ts
@@ -1,6 +1,6 @@
 import {InjectedConnector} from "@web3-react/injected-connector";
 
-export const supportedChainIds = [250, 31337];
+export const supportedChainIds = [4, 250, 31337];
 
 export const injected = new InjectedConnector({
     supportedChainIds,
