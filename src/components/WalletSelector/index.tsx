e2523c856242fda2d5d803937932aef587fc8233
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-26 08:20:28 +1100

    🎉 Update

--- src/components/WalletSelector/index.tsx
+++ src/components/WalletSelector/index.tsx
@@ -23,13 +23,13 @@ export default function WalletSelector() {
         <div className={`${walletSelector ? "" : "hidden"} bg-black bg-opacity-80 fixed inset-0 flex items-center justify-center z-50 modal`}>
             <div className="mx-auto lg:w-2/5 w-4/5 min-w-min bg-neutral-900 glow rounded-xl p-6 space-y-6">
                 <div className="flex items-center justify-between">
-                    <h2 className="text-white font-bold text-2xl">Choose a wallet:</h2>
+                    <h2 className="text-neutral-400 font-bold text-2xl">Choose a wallet:</h2>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className="w-8 cursor-pointer"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke="white"
+                        stroke="#a3a3a3"
                         onClick={() => setWalletSelector(false)}
                     >
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M6 18L18 6M6 6l12 12" />
