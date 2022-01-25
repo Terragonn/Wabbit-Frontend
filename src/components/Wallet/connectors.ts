4d553e450f06e3189b4c5dac72b7ebef4fe1a91f
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-25 11:53:07 +1100

    🎉 Update

--- src/components/Wallet/connectors.ts
+++ src/components/Wallet/connectors.ts
@@ -1,6 +1,15 @@
 import {InjectedConnector} from "@web3-react/injected-connector";
+import {WalletConnectConnector} from "@web3-react/walletconnect-connector";
 import {SUPPORTED_CHAIN_IDS} from "../../utils/useChainData";
 
 export const injected = new InjectedConnector({
     supportedChainIds: [...SUPPORTED_CHAIN_IDS],
+});
+
+export const walletConnect = new WalletConnectConnector({
+    rpc: {
+        250: "https://rpc.ftm.tools/",
+    },
+    bridge: "https://bridge.walletconnect.org",
+    qrcode: true,
 });
