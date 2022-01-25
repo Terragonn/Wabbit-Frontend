f2d1d47cb9c120cab7fc60258aca493592b907bd
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-25 14:43:13 +1100

    🎉 Update

--- .old/utils/loadERC20.ts
+++
@@ -1,7 +0,0 @@
-import { ethers } from "ethers";
-import ERC20 from "../config/ERC20.json";
-
-// Return an ERC20 contract for the given address
-export default function loadERC20(address: string, signer: ethers.providers.JsonRpcSigner) {
-    return new ethers.Contract(address, ERC20.abi, signer);
-}
