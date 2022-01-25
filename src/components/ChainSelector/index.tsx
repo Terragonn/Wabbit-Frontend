bcec49394b94fda9949bb826f008e1949981dbc1
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-25 21:58:36 +1100

    🎉 Update

--- src/components/ChainSelector/index.tsx
+++ src/components/ChainSelector/index.tsx
@@ -1,11 +1,8 @@
 import {useWeb3React} from "@web3-react/core";
-import {useEffect} from "react";
 import {SupportedChainIds} from "../../utils/useChainData";
 
 export default function ChainSelector() {
     const {chainId} = useWeb3React();
-
-    useEffect(() => {}, [chainId]);
 
     const chainConfig: {[key in SupportedChainIds | 0]: {name: string; color: string}} = {
         0: {name: "Invalid", color: "text-neutral-500"},
@@ -15,12 +12,23 @@ export default function ChainSelector() {
     };
 
     return (
-        <div className="bg-neutral-900 lg:px-4 px-2 lg:py-4 py-2 rounded-xl font-bold glow">
-            <select className="text-white font-bold bg-transparent border-transparent lg:text-3xl text-2xl rounded-xl w-full text-center">
-                <option>Invalid</option>
-                <option>Fantom</option>
-                <option>Rinkeby</option>
-                <option>Localhost</option>
+        <div className="bg-neutral-900 lg:py-4 py-2 rounded-xl font-bold glow">
+            <select
+                className={`mr-4 font-bold bg-transparent border-transparent lg:text-3xl text-2xl rounded-xl w-full text-center ${
+                    chainId && Object.keys(chainConfig).includes(chainId.toString()) ? chainConfig[chainId as SupportedChainIds | 0].color : chainConfig[0].color
+                }`}
+            >
+                {Object.entries(chainConfig).map(([key, value]) => {
+                    return (
+                        <option
+                            selected={chainId?.toString() === key || (!chainId && key === "0")}
+                            disabled={true}
+                            className={`${value.color} font-medium bg-neutral-900`}
+                        >
+                            {value.name}
+                        </option>
+                    );
+                })}
             </select>
         </div>
     );
