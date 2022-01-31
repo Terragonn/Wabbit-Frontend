f4c2ad95bec785bad5ad3ca298aae45728765d79
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-01-31 12:54:39 +1100

    🎉 Update

--- src/components/WalletSelector/card.tsx
+++ src/components/WalletSelector/card.tsx
@@ -1,8 +1,8 @@
 export default function Card({name, icon, fn}: {name: string; icon: string; fn: () => any}) {
     return (
         <div className="flex items-center justify-between bg-zinc-800 hover:bg-zinc-700 cursor-pointer rounded-xl px-8 py-4 shadow-md" onClick={() => fn()}>
-            <p className="text-3xl font-bold text-white">{name}</p>
-            <img src={icon} alt={name} width={50} className="rounded-xl" />
+            <p className="text-3xl font-bold text-white w-full">{name}</p>
+            <img src={icon} alt={name} width={50} className="rounded-xl ml-5" />
         </div>
     );
 }
