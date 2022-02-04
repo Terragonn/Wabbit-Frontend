fda989eb99c4ae7181f254c2e41f0945931178cd
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-05 08:15:18 +1100

    🎉 Update

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -5,9 +5,9 @@ export default function Nav({setNavState}: {setNavState: (state: boolean) => voi
     return (
         <nav className="py-8 flex items-center xl:justify-end justify-between space-x-10">
             <div className="xl:hidden block cursor-pointer" onClick={(e) => setNavState(true)}>
-                <i className="px-5 py-1 my-1 bg-white glow rounded-md" />
-                <i className="px-5 py-1 my-1 bg-white glow rounded-md" />
-                <i className="px-5 py-1 my-1 bg-white glow rounded-md" />
+                <i className="block px-5 py-1 my-1 bg-white glow rounded-md" />
+                <i className="block px-5 py-1 my-1 bg-white glow rounded-md" />
+                <i className="block px-5 py-1 my-1 bg-white glow rounded-md" />
             </div>
             <div className="flex items-center justify-evenly space-x-10">
                 <ChainSelector />
