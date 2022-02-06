d7328dc97ea3c9d663fb116352ebdffe0e0bcc55
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-07 08:28:34 +1100

    🎉 Update

--- src/components/ChainSelector/index.tsx
+++ src/components/ChainSelector/index.tsx
@@ -1,5 +1,5 @@
 import {useWeb3React} from "@web3-react/core";
-import {SupportedChainIds} from "../../utils/useChainData";
+import {SupportedChainIds} from "../../utils/providers/useChainData";
 
 export default function ChainSelector() {
     const {chainId} = useWeb3React();
--- src/utils/getApproved.ts
+++ src/utils/getApproved.ts
@@ -1,4 +1,4 @@
-import {Approved, Config} from "./useChainData";
+import {Approved, Config} from "./providers/useChainData";
 
 export default function getApproved(config: Config, address: string): Approved | undefined {
     const item = config.approved.filter((approved) => approved.address.toLowerCase() === address.toLowerCase());
