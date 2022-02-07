47b7d58df6739fc8704ec5f2adb26f85571e4398
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-07 14:03:46 +1100

    🎉 Update

--- components/Wrapper/HeadWrapper/index.tsx
+++ components/Wrapper/HeadWrapper/index.tsx
@@ -1,23 +1,22 @@
 import Head from "next/head";
+import Script from "next/script";
 
 export default function HeadWrapper({children}: {children: any}) {
     return (
         <>
             <Head>
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-1GYTWEZJJG" />
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag() {
-                                dataLayer.push(arguments);
-                            }
-                            gtag("js", new Date());
+                <Script src="https://www.googletagmanager.com/gtag/js?id=G-1GYTWEZJJG" strategy="afterInteractive" />
+                <Script strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag() {
+                            dataLayer.push(arguments);
+                        }
+                        gtag("js", new Date());
 
-                            gtag("config", "G-1GYTWEZJJG");
-                        `,
-                    }}
-                />
+                        gtag("config", "G-1GYTWEZJJG");
+                    `}
+                </Script>
 
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="manifest" href="/manifest.json" />
