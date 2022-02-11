8b2a398c74e5c86ca847f73a7c0f325ce801c85f
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-11 20:36:19 +1100

    🎉 Update

--- components/Error/index.tsx
+++ components/Error/index.tsx
@@ -5,7 +5,7 @@ export default function Error() {
 
     return (
         <div
-            className={`text-center bg-red-600 p-5 font-bold overflow-x-auto text-white rounded-xl my-10 shadow-lg shadow-red-500 text-xl cursor-pointer ${
+            className={`text-center max-h-48 bg-red-700 hover:bg-red-600 p-5 font-bold overflow-auto text-white rounded-xl my-10 shadow-lg shadow-red-500 text-xl cursor-pointer ${
                 !error ? "hidden" : ""
             }`}
             onClick={(e) => setError(null)}
