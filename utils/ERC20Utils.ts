a0e667de91d0091996f54667cb3abcbe66a2fcc7
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-02-28 13:54:48 +1100

    🎉 Update

--- utils/ERC20Utils.ts
+++ utils/ERC20Utils.ts
@@ -1,11 +1,11 @@
 import {ethers} from "ethers";
 
 import {OVERRIDE} from "../providers/useProtocolMethods";
-import ERC20 from "../config/ERC20.json";
-import {ERC20 as ERC20Type} from "../typechain-types";
+import ERC20ABI from "../config/ERC20.json";
+import {ERC20Upgradeable} from "../typechain-types";
 
 export function loadERC20(token: string, signer: ethers.providers.JsonRpcSigner) {
-    return new ethers.Contract(token, ERC20.abi, signer) as ERC20Type;
+    return new ethers.Contract(token, ERC20ABI.abi, signer) as ERC20Upgradeable;
 }
 
 export async function approveERC20(token: string, amount: ethers.BigNumber, contractAddress: string, signer: ethers.providers.JsonRpcSigner) {
