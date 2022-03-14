bcc417cb97036f7ca5f68d3023c4774b771606e2
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-03-14 22:45:17 +1100

    🎉 Update

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -5,15 +5,13 @@ interface Placeholder {
 
 export default function Banner({placeholders}: {placeholders: Placeholder[]}) {
     return (
-        <div className="lg:block hidden">
-            <div className="bg-neutral-900 bg-opacity-75 px-16 py-10 flex items-center justify-evenly rounded-xl glow my-16">
-                {placeholders.map((placeholder, index) => (
-                    <div className="text-center" key={index}>
-                        <h2 className="text-neutral-500 font-bold text-2xl pb-3">{placeholder.title}</h2>
-                        <p className="text-white font-bold text-4xl">{placeholder.body}</p>
-                    </div>
-                ))}
-            </div>
+        <div className="lg:flex grid grid-cols-2 bg-neutral-900 bg-opacity-75 px-16 py-10 items-center justify-evenly rounded-xl glow my-16">
+            {placeholders.map((placeholder, index) => (
+                <div className="text-center" key={index}>
+                    <h2 className="text-neutral-500 font-bold lg:text-2xl text-xl pb-3">{placeholder.title}</h2>
+                    <p className="text-white font-bold lg:text-4xl text-3xl">{placeholder.body}</p>
+                </div>
+            ))}
         </div>
     );
 }
