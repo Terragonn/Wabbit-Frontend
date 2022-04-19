4cdb4e76ab5f6e84d1bddcb0f197dd9c02d52310
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-20 08:48:22 +1000

    🎉 Update

--- components/ConnectWallet/index.tsx
+++ components/ConnectWallet/index.tsx
@@ -4,6 +4,7 @@ import { useWeb3React } from "@web3-react/core";
 import { injected } from "../../connectors";
 
 // **** This needs to be reimplemented with mobx
+// **** Don't forget to add a button for switching accounts and stuff and when these different components should be rendered !
 
 export default function ConnectWallet() {
     const { account, activate, deactivate } = useWeb3React();
