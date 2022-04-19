419c965b0a0c1a5768fe3eb2fd4c5ea26c868e81
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-20 08:09:30 +1000

    🎉 Update

--- components/Nav/NavLinkRenderer/index.tsx
+++ components/Nav/NavLinkRenderer/index.tsx
@@ -1,1 +1,9 @@
-export default function NavLinkRenderer() {}
+import NavLink from "../NavLink";
+import { Eyeglass } from "tabler-icons-react";
+
+const data = [{ icon: <Eyeglass size={16} />, color: "blue", label: "Lens" }];
+
+export default function NavLinkRenderer() {
+    const links = data.map((link) => <NavLink {...link} key={link.label} />);
+    return <div>{links}</div>;
+}
--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,9 +1,12 @@
 import { Navbar } from "@mantine/core";
+import NavLinkRenderer from "./NavLinkRenderer";
 
 export default function Nav() {
     return (
         <Navbar p="xl">
-            <Navbar.Section grow>MainLinks</Navbar.Section>
+            <Navbar.Section grow>
+                <NavLinkRenderer />
+            </Navbar.Section>
             <Navbar.Section>Socials</Navbar.Section>
         </Navbar>
     );
