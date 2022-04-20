cd74b68e738029f0f1f30d78c7dd309aac1b5cc1
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-20 19:49:48 +1000

    🎉 Update

--- components/ConnectWallet/WalletCard/index.tsx
+++ components/ConnectWallet/WalletCard/index.tsx
@@ -4,20 +4,21 @@ export default function WalletCard({ name, imgURL, onClick }: { name: string; im
     return (
         <UnstyledButton
             onClick={onClick}
-            mt={24}
+            mt={18}
             sx={(theme) => ({
                 display: "block",
                 width: "100%",
                 padding: theme.spacing.xs,
                 borderRadius: theme.radius.sm,
                 color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+                backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
 
                 "&:hover": {
-                    backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
+                    backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[1],
                 },
             })}
         >
-            <Group position="apart" p="md">
+            <Group position="apart" p="sm">
                 <Text size="xl" weight={700}>
                     {name}
                 </Text>
