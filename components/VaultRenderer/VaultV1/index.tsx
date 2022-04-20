241ae5f64b78c5dfe96fd817b72274c5480b7187
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-20 22:57:04 +1000

    🎉 Update

--- components/VaultRenderer/VaultV1/index.tsx
+++ components/VaultRenderer/VaultV1/index.tsx
@@ -1,3 +1,7 @@
-export default function VaultV1() {
+export default function VaultV1({}: { contract: string }) {
+    // **** I need some way of getting a list of tokens that the user can use
+    // **** When they click on the vault it will toggle the modal which will show all of the options they have to interact with the vault
+    // **** Should also be some sort of agreement
+
     return <></>;
 }
