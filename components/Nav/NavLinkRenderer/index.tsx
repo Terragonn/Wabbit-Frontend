dc0af0e00ecd934108d44b963570f8e826d2cf42
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-20 21:22:51 +1000

    🎉 Update

--- components/Nav/NavLinkRenderer/index.tsx
+++ components/Nav/NavLinkRenderer/index.tsx
@@ -4,8 +4,8 @@ import { Eyeglass, BuildingBank } from "tabler-icons-react";
 import NavLink from "../NavLink";
 
 const data = [
-    { icon: <Eyeglass size={16} />, color: "indigo", label: "Lens", href: "" },
-    { icon: <BuildingBank size={16} />, color: "grape", label: "Lever", href: "" },
+    { icon: <Eyeglass size={16} />, color: "indigo", label: "Lens", href: "lens" },
+    { icon: <BuildingBank size={16} />, color: "grape", label: "Lever", href: "lever" },
 ];
 
 export default function NavLinkRenderer() {
