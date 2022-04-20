c839b5c9fd5c7dbeb91cfbac6942ac8b07ff2069
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-20 21:59:58 +1000

    🎉 Update

--- _pages/LensPage/index.tsx
+++ _pages/LensPage/index.tsx
@@ -1,10 +1,19 @@
-import { Text, Title } from "@mantine/core";
+import { Box, Text, Title } from "@mantine/core";
 
 export default function LensPage() {
     return (
         <>
-            <Title order={2}>Torque Lens</Title>
-            <Text color="dimmed">Hello world</Text>
+            <Box
+                pb="sm"
+                sx={(theme) => ({
+                    borderBottom: `1px solid ${theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]}`,
+                })}
+            >
+                <Title order={2} mb={6}>
+                    Torque Lens
+                </Title>
+                <Text color="dimmed">Hello world</Text>
+            </Box>
         </>
     );
 }
