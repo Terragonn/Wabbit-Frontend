ab1677c1f321b94d451c8a1d892a7330e7790a3b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-20 19:56:00 +1000

    🎉 Update

--- components/ConnectWallet/index.tsx
+++ components/ConnectWallet/index.tsx
@@ -27,17 +27,26 @@ export default function ConnectWallet() {
                 <WalletCard
                     name="Metamask"
                     imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/800px-MetaMask_Fox.svg.png"
-                    onClick={() => activate(injected)}
+                    onClick={async () => {
+                        await activate(injected);
+                        setOpened(false);
+                    }}
                 />
                 <WalletCard
                     name="WalletConnect"
                     imgURL="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/bftsslxvhe2yaih6nyl9"
-                    onClick={() => activate(walletConnect(defaultChainId))}
+                    onClick={async () => {
+                        await activate(walletConnect(defaultChainId));
+                        setOpened(false);
+                    }}
                 />
                 <WalletCard
                     name="WalletLink"
                     imgURL="https://play-lh.googleusercontent.com/wrgUujbq5kbn4Wd4tzyhQnxOXkjiGqq39N4zBvCHmxpIiKcZw_Pb065KTWWlnoejsg"
-                    onClick={() => activate(walletLink(defaultChainId))}
+                    onClick={async () => {
+                        await activate(walletLink(defaultChainId));
+                        setOpened(false);
+                    }}
                 />
             </Modal>
 
