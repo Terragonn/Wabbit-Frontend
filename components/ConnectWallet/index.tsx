b0c1c5f02cadc256899f900ddd0758f705965ec0
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-20 19:17:59 +1000

    🎉 Update

--- components/ConnectWallet/index.tsx
+++ components/ConnectWallet/index.tsx
@@ -1,26 +1,18 @@
-import { Button } from "@mantine/core";
-import { useWeb3React } from "@web3-react/core";
-
-import { injected } from "../../connectors";
-
-// **** I need a new seperate sort of wallet provider for this probably using mobX
+import { Button, Modal } from "@mantine/core";
+import { useState } from "react";
 
 export default function ConnectWallet() {
-    const { account, activate } = useWeb3React();
-
-    async function connectInjected() {
-        await activate(injected);
-    }
+    const [opened, setOpened] = useState<boolean>(false);
 
     return (
         <>
-            {account ? (
-                account
-            ) : (
-                <Button variant="gradient" onClick={connectInjected} gradient={{ from: "indigo", to: "grape", deg: 45 }}>
-                    Connect
-                </Button>
-            )}
+            <Modal opened={opened} onClose={() => setOpened(false)}>
+                Hello World
+            </Modal>
+
+            <Button onClick={() => setOpened(true)} variant="gradient" gradient={{ from: "indigo", to: "grape", deg: 45 }}>
+                Connect
+            </Button>
         </>
     );
 }
