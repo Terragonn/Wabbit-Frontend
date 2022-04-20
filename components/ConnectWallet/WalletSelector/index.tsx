10119ea15e51ff7d0ae2ec61926fbcc2265d70ac
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-20 20:16:40 +1000

    🎉 Update

---
+++ components/ConnectWallet/WalletSelector/index.tsx
@@ -0,0 +1,39 @@
+import { useWeb3React } from "@web3-react/core";
+import { injected, walletConnect, walletLink } from "../../../connectors";
+import { SupportedChainId } from "../../../utils/ChainData";
+import WalletCard from "../WalletCard";
+
+export default function WalletSelector({ chainId, closeModal }: { chainId: SupportedChainId; closeModal: () => void }) {
+    const { activate } = useWeb3React();
+
+    function connectWrapper(connect: () => Promise<void>) {
+        return async () => {
+            try {
+                await connect();
+                closeModal();
+            } catch (e) {
+                console.log(e);
+            }
+        };
+    }
+
+    return (
+        <>
+            <WalletCard
+                name="Metamask"
+                imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/800px-MetaMask_Fox.svg.png"
+                onClick={connectWrapper(async () => await activate(injected, undefined, true))}
+            />
+            <WalletCard
+                name="WalletConnect"
+                imgURL="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/bftsslxvhe2yaih6nyl9"
+                onClick={connectWrapper(async () => await activate(walletConnect(chainId), undefined, true))}
+            />
+            <WalletCard
+                name="WalletLink"
+                imgURL="https://play-lh.googleusercontent.com/wrgUujbq5kbn4Wd4tzyhQnxOXkjiGqq39N4zBvCHmxpIiKcZw_Pb065KTWWlnoejsg"
+                onClick={connectWrapper(async () => await activate(walletLink(chainId), undefined, true))}
+            />
+        </>
+    );
+}
--- components/ConnectWallet/index.tsx
+++ components/ConnectWallet/index.tsx
@@ -1,17 +1,16 @@
 import { Box, Button, Modal, Text } from "@mantine/core";
 import { useState } from "react";
 
-import WalletCard from "./WalletCard";
-import { injected, walletConnect, walletLink } from "../../connectors";
 import { useWeb3React } from "@web3-react/core";
+import WalletSelector from "./WalletSelector";
 import { SupportedChainId } from "../../utils/ChainData";
 
 export default function ConnectWallet() {
-    const { active, activate, chainId } = useWeb3React();
+    const { active, chainId } = useWeb3React();
 
     const [opened, setOpened] = useState<boolean>(false);
 
-    const defaultChainId: SupportedChainId = 250;
+    const selectedChainId: SupportedChainId = 250;
 
     return (
         <>
@@ -24,35 +23,14 @@ export default function ConnectWallet() {
                 >
                     <Text size="md">Choose A Wallet</Text>
                 </Box>
-                <WalletCard
-                    name="Metamask"
-                    imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/800px-MetaMask_Fox.svg.png"
-                    onClick={async () => {
-                        await activate(injected);
-                        setOpened(false);
-                    }}
-                />
-                <WalletCard
-                    name="WalletConnect"
-                    imgURL="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/bftsslxvhe2yaih6nyl9"
-                    onClick={async () => {
-                        await activate(walletConnect(defaultChainId));
-                        setOpened(false);
-                    }}
-                />
-                <WalletCard
-                    name="WalletLink"
-                    imgURL="https://play-lh.googleusercontent.com/wrgUujbq5kbn4Wd4tzyhQnxOXkjiGqq39N4zBvCHmxpIiKcZw_Pb065KTWWlnoejsg"
-                    onClick={async () => {
-                        await activate(walletLink(defaultChainId));
-                        setOpened(false);
-                    }}
-                />
+                <WalletSelector chainId={selectedChainId} closeModal={() => setOpened(false)} />
             </Modal>
 
             <Button onClick={() => setOpened(true)} variant="gradient" gradient={{ from: "indigo", to: "grape", deg: 45 }}>
                 Connect
             </Button>
+
+            {/* **** Now I need some option that checks if the correct card has been selected */}
         </>
     );
 }
