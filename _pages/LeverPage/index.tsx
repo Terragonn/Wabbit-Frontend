f6ec752e703aa6b3a24d9ecb28b52dfb2a37aa87
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 13:18:47 +1000

    🎉 Update

--- _pages/LeverPage/index.tsx
+++ _pages/LeverPage/index.tsx
@@ -1,3 +1,18 @@
+import { Group, Text, Title } from "@mantine/core";
+import { Backhoe } from "tabler-icons-react";
+
 export default function LeverPage() {
-    return <>Lever page</>;
+    return (
+        <>
+            <Title pt="xl" align="center" order={2}>
+                Under Construction
+            </Title>
+            <Text align="center" size="lg">
+                This page is still under construction. Come back soon!
+            </Text>
+            <Group position="center" mt="xl">
+                <Backhoe size={200} />
+            </Group>
+        </>
+    );
 }
