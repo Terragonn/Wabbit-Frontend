e59552e2da4c5deca5cd6f523b29fb9e79f20fd7
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-21 13:13:58 +1000

    🎉 Update

--- components/VaultRenderer/index.tsx
+++ components/VaultRenderer/index.tsx
@@ -1,17 +1,20 @@
+import { Grid, Group } from "@mantine/core";
 import VaultV1 from "./VaultV1";
 
 export default function VaultRenderer() {
     return (
-        <>
-            <VaultV1
-                name="Beefy FTM-USDC LP"
-                description="Torque USDC-FTM LP strategy optimized on-chain over different vaults on Beefy Finance to earn you the highest yield possible with the least effort on your behalf!"
-                tags={["New", "Low Risk"]}
-                color={"blue"}
-                apy={24.79}
-                tvl={102.3}
-                fee={0}
-            />
-        </>
+        <Grid>
+            <Grid.Col span={6}>
+                <VaultV1
+                    name="Beefy FTM-USDC LP"
+                    description="Torque USDC-FTM LP strategy optimized on-chain over different vaults on Beefy Finance to earn you the highest yield possible with the least effort on your behalf!"
+                    tags={["New", "Low Risk"]}
+                    color={"blue"}
+                    apy={24.79}
+                    tvl={102.3}
+                    fee={0}
+                />
+            </Grid.Col>
+        </Grid>
     );
 }
