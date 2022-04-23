65403091e73d3ac9adc6ee2c6f1bd001fff7c536
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-23 17:35:18 +1000

    🎉 Update

--- src/providers/WalletProvider/index.tsx
+++ src/providers/WalletProvider/index.tsx
@@ -4,3 +4,5 @@ import { ethers } from "ethers";
 export default function WalletProvider({ children }: { children: any }) {
     return <Web3ReactProvider getLibrary={(provider) => new ethers.providers.Web3Provider(provider)}>{children}</Web3ReactProvider>;
 }
+
+// **** We need to do more work in this one to avoid problems
