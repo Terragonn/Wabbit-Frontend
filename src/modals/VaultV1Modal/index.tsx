60e932e01d3eb3cc51b3b7a00ce3062516fb6a19
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-23 21:45:00 +1000

    🎉 Update

--- src/modals/VaultV1Modal/index.tsx
+++ src/modals/VaultV1Modal/index.tsx
@@ -1,7 +1,10 @@
 import { Box, Button, Group, Modal, NumberInput, Text } from "@mantine/core";
+import { useState } from "react";
 import { Token } from "../../utils";
 
-export default function VaultV1Modal({ token, opened, onClose }: { token: Token[]; opened: boolean; onClose: () => void }) {
+export default function VaultV1Modal({ token, name, opened, onClose }: { token: Token[]; name: string; opened: boolean; onClose: () => void }) {
+    const [tokenAmount, setTokenAmount] = useState<{ [key: string]: number }>({});
+
     return (
         <Modal opened={opened} onClose={onClose}>
             <Box
@@ -10,13 +13,32 @@ export default function VaultV1Modal({ token, opened, onClose }: { token: Token[
                     borderBottom: `1px solid ${theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]}`,
                 })}
             >
-                <Text size="md">Vault</Text>
+                <Text size="md">
+                    <Text weight={700} component="span">
+                        {name}
+                    </Text>{" "}
+                    Vault
+                </Text>
             </Box>
             {token.map((tkn, index) => (
-                // **** Have an approve button next to this
-                // **** Need a good way of updating this too
                 <Group grow>
-                    <NumberInput key={index} mt="lg" label={tkn.name + " (" + tkn.ticker + ")"} placeholder="0.0" defaultValue={0} size="md" hideControls />
+                    <NumberInput
+                        key={index}
+                        mt="lg"
+                        label={tkn.name + " (" + tkn.ticker + ")"}
+                        placeholder="0.0"
+                        size="md"
+                        hideControls
+                        // **** This should probably be controlled instead... ?
+                        min={0}
+                        max={9999999}
+                        onChange={(num) =>
+                            setTokenAmount((tknAmnt) => {
+                                tknAmnt[tkn.address] = num ? num : 0;
+                                return tknAmnt;
+                            })
+                        }
+                    />
                 </Group>
             ))}
             <Group grow mt="xl">
