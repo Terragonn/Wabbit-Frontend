b2a1f51988cbee145c260b8b24bb42f0549b317c
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-24 17:59:14 +1000

    🎉 Update

--- src/modals/VaultV1Modal/Withdraw.tsx
+++ src/modals/VaultV1Modal/Withdraw.tsx
@@ -2,7 +2,7 @@ import { Box, Text } from "@mantine/core";
 
 export default function Withdraw() {
     return (
-        <Box mt="xl">
+        <Box mt="sm">
             <Text size="xl" weight={700}>
                 Withdraw
             </Text>
