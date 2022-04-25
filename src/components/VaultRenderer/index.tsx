a2a89268f4d9bfbc9c1d3c5899d57c642b89ae7b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-25 12:26:56 +1000

    🎉 Update

--- src/components/VaultRenderer/index.tsx
+++ src/components/VaultRenderer/index.tsx
@@ -12,7 +12,7 @@ export default function VaultRenderer() {
                     name="Beefy FTM-USDC LP"
                     description="Torque USDC-FTM LP strategy optimized on-chain over different vaults on Beefy Finance to earn you the highest yield possible with the least effort on your behalf!"
                     token={[TokenData.FTM, TokenData.USDC]}
-                    tags={["New", "Low Risk"]}
+                    tags={["New", "Blue Chip"]}
                     color={"blue"}
                 />
             </Grid.Col>
