b9e496bde35fdd282bdcddc2272fb67750c3d5dc
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-28 22:28:35 +1000

    🎉 Update

--- src/pages/lever.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-
-import { ConstructionPage } from "../_pages";
-
-const Lever: NextPage = () => {
-    return <ConstructionPage />;
-};
-
-export default Lever;
--- src/pages/pawn.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-
-import { ConstructionPage } from "../_pages";
-
-const Pawn: NextPage = () => {
-    return <ConstructionPage />;
-};
-
-export default Pawn;
