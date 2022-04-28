baccb2ca4dc8a54d374439a4d5c53e00a58f7e75
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-28 22:38:22 +1000

    🎉 Update

--- src/head/LensHead/index.tsx
+++ src/head/LensHead/index.tsx
@@ -1,1 +1,11 @@
-export default function LensHead() {}
+import Head from "next/head";
+
+export default function LensHead() {
+    return (
+        <Head>
+            <title>Lens - Torque Money</title>
+
+            <link rel="canonical" href="https://app.torque.money/lens" />
+        </Head>
+    );
+}
