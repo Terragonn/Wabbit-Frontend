015108071b3dd8a28d636db6040a0f554a88836e
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-28 22:26:00 +1000

    🎉 Update

--- src/head/BaseHead/index.tsx
+++ src/head/BaseHead/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-export default function Base() {
+export default function BaseHead() {
     return (
         <Head>
             <link rel="icon" href="/images/favicon.ico" />
--- src/layout/Default/index.tsx
+++ src/layout/Default/index.tsx
@@ -1,14 +1,18 @@
 import { Shell } from "../../components";
+import { BaseHead } from "../../head";
 import { WalletProvider, CSSProvider, WalletAutoConnectProvider } from "../../providers";
 
 export default function DefaultLayout({ children }: { children: any }) {
     return (
-        <WalletProvider>
-            <WalletAutoConnectProvider>
-                <CSSProvider>
-                    <Shell>{children}</Shell>
-                </CSSProvider>
-            </WalletAutoConnectProvider>
-        </WalletProvider>
+        <>
+            <BaseHead />
+            <WalletProvider>
+                <WalletAutoConnectProvider>
+                    <CSSProvider>
+                        <Shell>{children}</Shell>
+                    </CSSProvider>
+                </WalletAutoConnectProvider>
+            </WalletProvider>
+        </>
     );
 }
