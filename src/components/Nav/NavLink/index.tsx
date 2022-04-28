b810405d9fbbd4ec14feae34af584de33a3ce762
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-28 22:33:13 +1000

    🎉 Update

--- src/components/Nav/NavLink/index.tsx
+++ src/components/Nav/NavLink/index.tsx
@@ -2,32 +2,54 @@ import { Anchor, Box, Group, Text, ThemeIcon, UnstyledButton } from "@mantine/co
 import Link from "next/link";
 import { ReactNode } from "react";
 
-export default function NavLink({ icon, color, label, href, mt }: { icon: ReactNode; color: string; label: string; href: string; mt: string }) {
+export default function NavLink({
+    icon,
+    color,
+    label,
+    href,
+    mt,
+    disabled = false,
+}: {
+    icon: ReactNode;
+    color: string;
+    label: string;
+    href: string;
+    mt: string;
+    disabled?: boolean;
+}) {
+    const link = (
+        <UnstyledButton
+            sx={(theme) => ({
+                display: "block",
+                width: "100%",
+                padding: theme.spacing.xs,
+                borderRadius: theme.radius.sm,
+                color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+
+                "&:hover": {
+                    backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
+                },
+            })}
+        >
+            <Group>
+                <ThemeIcon color={color} variant="light">
+                    {icon}
+                </ThemeIcon>
+
+                <Text size="sm">{label}</Text>
+            </Group>
+        </UnstyledButton>
+    );
+
     return (
         <Box mt={mt}>
-            <Anchor href={href} component={Link}>
-                <UnstyledButton
-                    sx={(theme) => ({
-                        display: "block",
-                        width: "100%",
-                        padding: theme.spacing.xs,
-                        borderRadius: theme.radius.sm,
-                        color: theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
-
-                        "&:hover": {
-                            backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
-                        },
-                    })}
-                >
-                    <Group>
-                        <ThemeIcon color={color} variant="light">
-                            {icon}
-                        </ThemeIcon>
-
-                        <Text size="sm">{label}</Text>
-                    </Group>
-                </UnstyledButton>
-            </Anchor>
+            {disabled ? (
+                link
+            ) : (
+                <Anchor href={href} component={Link}>
+                    {link}
+                </Anchor>
+            )}
         </Box>
     );
 }
--- src/components/Nav/NavLinkRenderer/index.tsx
+++ src/components/Nav/NavLinkRenderer/index.tsx
@@ -6,8 +6,8 @@ export default function NavLinkRenderer() {
     return (
         <>
             <NavLink icon={<Eyeglass size={16} />} color={"indigo"} label="Lens" href="lens" mt="xl" />
-            <NavLink icon={<BuildingBank size={16} />} color={"grape"} label="Lever" href="lever" mt="xl" />
-            <NavLink icon={<Coin size={16} />} color={"blue"} label="Pawn" href="pawn" mt="xl" />
+            <NavLink icon={<BuildingBank size={16} />} color={"grape"} label="Lever" href="lever" mt="xl" disabled={true} />
+            <NavLink icon={<Coin size={16} />} color={"blue"} label="Pawn" href="pawn" mt="xl" disabled={true} />
         </>
     );
 }
