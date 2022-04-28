4e70da7966d70e24764e39441cd8a85391575bd9
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-29 08:29:28 +1000

    🎉 Update

--- src/modals/VaultV1Modal/Deposit.tsx
+++ src/modals/VaultV1Modal/Deposit.tsx
@@ -1,16 +1,33 @@
-import { Button, Group } from "@mantine/core";
+import { Box, Button, Group } from "@mantine/core";
 import { useWeb3React } from "@web3-react/core";
 
 import VaultInput from "./VaultInput";
 import { Token } from "../../utils";
+import { useState } from "react";
+import { Input } from "../../components";
 
 export default function Deposit({ token, vault }: { token: Token[]; vault: string }) {
     const { account, library } = useWeb3React();
 
+    const [tokenAmount, setTokenAmount] = useState<{ [key: string]: number }>(() => {
+        const tmp: { [key: string]: number } = {};
+        token.forEach((tkn) => (tmp[tkn.address] = 0));
+        return tmp;
+    });
+
     if (account && library)
         return (
             <>
-                {account && library && <VaultInput token={token} account={account} vault={vault} library={library} />}
+                {account && library && (
+                    <Box>
+                        {token.map((tkn, index) => (
+                            <Box key={index} mb="md">
+                                <Input token={tkn} account={account} vault={vault} library={library} />
+                            </Box>
+                        ))}
+                    </Box>
+                )}
+
                 <Group grow mt="lg">
                     <Button variant="gradient" size="lg" gradient={{ from: "pink", to: "grape", deg: 45 }}>
                         Deposit
--- src/modals/VaultV1Modal/VaultInput.tsx
+++ src/modals/VaultV1Modal/VaultInput.tsx
@@ -4,23 +4,4 @@ import { Token } from "../../utils";
 import { Input } from "../../components";
 import { Box } from "@mantine/core";
 
-export default function VaultInput({ token, account, vault, library }: { token: Token[]; account: string; vault: string; library: any }) {
-    const [tokenAmount, setTokenAmount] = useState<{ [key: string]: number }>(() => {
-        const tmp: { [key: string]: number } = {};
-        token.forEach((tkn) => (tmp[tkn.address] = 0));
-        return tmp;
-    });
-
-    // **** I need to set up some sort of registry with this where we update everytime this happens - we also
-    // need a way of making sure that the deposit button doesnt come up until we approve all
-
-    return (
-        <Box>
-            {token.map((tkn, index) => (
-                <Box key={index} mb="md">
-                    <Input token={tkn} account={account} vault={vault} library={library} />
-                </Box>
-            ))}
-        </Box>
-    );
-}
+export default function VaultInput({ token, account, vault, library }: { token: Token[]; account: string; vault: string; library: any }) {}
