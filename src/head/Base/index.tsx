aef5f82256fed21d5f9ee86fe4c7664443d5d29d
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-28 22:21:53 +1000

    🎉 Update

---
+++ src/head/Base/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export default function Base() {
+    return (
+        <Head>
+            <link rel="icon" href="/images/favicon.ico" />
+            <link rel="manifest" href="/manifest.json" />
+
+            <title>Torque Money - DeFi The Norm</title>
+            <meta name="description" content="The official dapp of Torque Money. Maximize your DeFi yields with the least effort possible!" />
+
+            <meta name="theme-color" content="#921ebb" />
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="robots" content="index, follow" />
+            <link rel="canonical" href="https://app.torque.money" />
+        </Head>
+    );
+}
