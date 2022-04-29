76cdbbd47f872878d183deaf53ffaa2dc87266dd
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-29 19:31:09 +1000

    🎉 Update

--- src/head/BaseHead/index.tsx
+++ src/head/BaseHead/index.tsx
@@ -1,8 +1,20 @@
 import Head from "next/head";
+import Script from "next/script";
 
 export default function BaseHead() {
     return (
         <Head>
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-M831HRNWYT" strategy="afterInteractive" />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){window.dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-M831HRNWYT');
+                `}
+            </Script>
+
             <link rel="icon" href="/images/favicon.ico" />
             <link rel="manifest" href="/manifest.json" />
 
