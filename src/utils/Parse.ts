d8b2cfe0bb7df108cb3b7942108045ee4d8f8c30
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-29 20:35:40 +1000

    🎉 Update

--- src/utils/Parse.ts
+++ src/utils/Parse.ts
@@ -2,10 +2,25 @@ import { BigNumber } from "ethers";
 
 import { ROUND_NUMBER } from ".";
 
+// Format a number to given decimal places
+export function formatNumber(raw: number) {
+    return raw.toFixed(2);
+}
+
+// Parse a number into its shortened form
+export function parseNumber(raw: number) {
+    if (raw > 1e9) return formatNumber(raw / 1e9) + "B";
+    else if (raw > 1e6) return formatNumber(raw / 1e6) + "M";
+    else if (raw > 1e3) return formatNumber(raw / 1e3) + "K";
+    else return formatNumber(raw);
+}
+
+// Parse an address to be compatible with all addresses
 export function parseAddress(address: string) {
     return address.toLowerCase();
 }
 
+// Parse a number to a big number
 export function parseToBigNumber(raw: number, decimals: number) {
     return BigNumber.from(10)
         .pow(decimals)
---
+++ src/utils/VaultAPIData.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+// Get the vault APY
+export async function vaultAPY(vault: string) {}
+
+// Get the vault user TVL
+export async function vaultUserTVL(vault: string) {}
+
+// Get the vault TVL
+export async function vaultTVL(vault: string) {}
+
+// Get the vault fee
+export async function vaultFeePercent(vault: string) {}
