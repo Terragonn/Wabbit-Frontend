1fb623964d7abdc1fc0a1ccbc0a32871fe2d54fa
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-29 10:52:58 +1000

    🎉 Update

--- src/components/VaultV1/Data.tsx
+++ src/components/VaultV1/Data.tsx
@@ -2,35 +2,31 @@ import { Box, Group, Text } from "@mantine/core";
 
 export default function Data({ vault }: { vault: string }) {
     return (
-        <>
-            <Group position="apart">
-                <Box>
-                    <Text size="lg" color="dimmed">
-                        APY
-                    </Text>
-                    <Text size="lg" weight={700}>
-                        {/* {apy} % */}
-                    </Text>
-                </Box>
-                <Box>
-                    <Text size="lg" color="dimmed">
-                        TVL
-                    </Text>
-                    <Text size="lg" weight={700}>
-                        {/* $ {tvl} K */}
-                    </Text>
-                </Box>
-            </Group>
-            <Group position="apart" mt="md">
-                <Box>
-                    <Text size="lg" color="dimmed">
-                        Fee
-                    </Text>
-                    <Text size="lg" weight={700}>
-                        {/* {fee} % */}
-                    </Text>
-                </Box>
-            </Group>
-        </>
+        <Group position="apart">
+            <Text size="lg" color="dimmed">
+                APY:{" "}
+                <Text size="lg" weight={700} component="span" color="gray">
+                    30 %
+                </Text>
+            </Text>
+            <Text size="lg" color="dimmed">
+                Balance:{" "}
+                <Text size="lg" weight={700} component="span" color="gray">
+                    $ 250
+                </Text>
+            </Text>
+            <Text size="lg" color="dimmed">
+                TVL:{" "}
+                <Text size="lg" weight={700} component="span" color="gray">
+                    $ 250.36 K
+                </Text>
+            </Text>
+            <Text size="lg" color="dimmed">
+                Fee:{" "}
+                <Text size="lg" weight={700} component="span" color="gray">
+                    5 %
+                </Text>
+            </Text>
+        </Group>
     );
 }
