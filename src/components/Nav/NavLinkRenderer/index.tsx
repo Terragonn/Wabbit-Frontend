ed5a6464b4ef94173cab5d73d0023fea9b7f47ae
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-01 08:48:53 +1000

    🎉 Update

--- src/components/Nav/NavLinkRenderer/index.tsx
+++ src/components/Nav/NavLinkRenderer/index.tsx
@@ -1,4 +1,4 @@
-import { Eyeglass, BuildingBank, Coin } from "tabler-icons-react";
+import { Eyeglass, Coin, Planet, BuildingBank } from "tabler-icons-react";
 
 import NavLink from "../NavLink";
 
@@ -6,8 +6,9 @@ export default function NavLinkRenderer() {
     return (
         <>
             <NavLink icon={<Eyeglass size={16} />} color={"indigo"} label="Lens" href="lens" mt="xl" />
-            <NavLink icon={<BuildingBank size={16} />} color={"grape"} label="Lever" href="lever" mt="xl" disabled={true} />
             <NavLink icon={<Coin size={16} />} color={"blue"} label="Pawn" href="pawn" mt="xl" disabled={true} />
+            <NavLink icon={<Planet size={16} />} color={"grape"} label="Portal" href="portal" mt="xl" disabled={true} />
+            <NavLink icon={<BuildingBank size={16} />} color={"cyan"} label="Lever" href="lever" mt="xl" disabled={true} />
         </>
     );
 }
