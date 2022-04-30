62b08cc29960981c6e6024f5288626119389a7a1
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-04-30 10:49:51 +1000

    🎉 Update

--- src/hooks/VaultModal/DepositData.ts
+++ src/hooks/VaultModal/DepositData.ts
@@ -1,23 +1,52 @@
 import { useEffect, useState } from "react";
-import { getTokenDataByAddress, Token } from "../../utils";
+
+import { formatNumber, getTokenDataByAddress, Token } from "../../utils";
+import { usePrice } from "..";
 
 export function useDepositData(tokenAmount: { [key: string]: number }) {
-    const [total, setTotal] = useState<string>("$ 0.00");
-    const [breakdown, setBreakdown] = useState<[Token, string][]>(getBreakdown);
+    const getPrice = usePrice();
 
-    function getBreakdown() {
+    const [total, setTotal] = useState<string>("$ 0.00");
+    const [breakdown, setBreakdown] = useState<[Token, string][]>(() => {
         const pairs = Object.entries(tokenAmount);
-        // **** I need to get the prices properly of course
         return pairs.map((pair) => [getTokenDataByAddress(pair[0]), "$ 0.00"] as [Token, string]);
+    });
+
+    async function getBreakdown() {
+        const pairs = Object.entries(tokenAmount);
+
+        const out: [Token, string][] = [];
+        for (const pair of pairs) {
+            const token = getTokenDataByAddress(pair[0]);
+
+            const unitPrice = await getPrice(token);
+            let price = "$ 0.00";
+            if (unitPrice) price = "$ " + formatNumber(unitPrice * tokenAmount[token.address]);
+
+            out.push([token, price]);
+        }
+
+        return out;
     }
 
     useEffect(() => {
-        // Update the total with the prices
+        console.log("Updated");
+
+        (async () => {
+            let totalRaw = 0;
+
+            for (const tkn of Object.keys(tokenAmount)) {
+                const token = getTokenDataByAddress(tkn);
+                const price = await getPrice(token);
+                if (price) totalRaw += price;
+            }
+
+            setTotal("$ " + formatNumber(totalRaw));
+        })();
     }, [tokenAmount]);
 
     useEffect(() => {
-        // Update the breakdown with the prices
-        getBreakdown();
+        (async () => setBreakdown(await getBreakdown()))();
     }, [tokenAmount]);
 
     return { total, breakdown };
