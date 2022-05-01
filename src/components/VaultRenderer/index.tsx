ddec06e74818c10c3c6d54510e1366aca323fe71
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-01 12:05:18 +1000

    🎉 Update

--- src/components/VaultRenderer/index.tsx
+++ src/components/VaultRenderer/index.tsx
@@ -1,12 +1,22 @@
 import { Grid } from "@mantine/core";
+import { useEffect, useState } from "react";
 
 import { TokenData } from "../../utils";
 import { Vault } from "..";
+import { useBreakpoint } from "../../hooks";
 
 export default function VaultRenderer() {
+    const { ltSm } = useBreakpoint();
+    const [span, setSpan] = useState<number>(6);
+
+    useEffect(() => {
+        if (ltSm) setSpan(12);
+        else setSpan(6);
+    }, [ltSm]);
+
     return (
         <Grid>
-            <Grid.Col span={6}>
+            <Grid.Col span={span}>
                 <Vault
                     vault="0x7b02CBA8c6bFAc6eBAb2dfA57096A9D60d2162Ae"
                     name="Beefy FTM-USDC LP"
