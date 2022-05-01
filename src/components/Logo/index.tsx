37d9ea7d54d2e2388d444cec1da49cbbfead4009
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-01 17:32:11 +1000

    🎉 Update

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,7 +3,7 @@ import { Text } from "@mantine/core";
 export default function Logo() {
     return (
         <Text component="span" align="center" variant="gradient" gradient={{ from: "indigo", to: "grape", deg: 45 }} size="xl" weight={700} transform="uppercase">
-            Torque Money
+            Torque
         </Text>
     );
 }
