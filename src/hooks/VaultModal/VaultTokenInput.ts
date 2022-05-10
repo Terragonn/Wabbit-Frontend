efa2b3af58cf58a94be100b9f7b50d703871c73c
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-10 20:07:10 +1000

    🎉 Update

--- src/hooks/VaultModal/VaultTokenInput.ts
+++ src/hooks/VaultModal/VaultTokenInput.ts
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
-import { Token, vaultBalance, parseAddress, parseToBigNumber, getTokenDataByAddress } from "../../utils";
+import { Token, vaultBalance, parseAddress, parseToBigNumber, getTokenDataByAddress, ROUND_NUMBER } from "../../utils";
 
 export function useVaultDeposit(token: Token[], vault: string) {
+    const [tokenBalance, setTokenBalance] = useState<{ [key: string]: number } | null>(null);
     const [tokenAmount, setTokenAmount] = useState<{ [key: string]: number }>(() => {
         const tmp: { [key: string]: number } = {};
         token.forEach((tkn) => (tmp[tkn.address] = 0));
@@ -13,20 +14,22 @@ export function useVaultDeposit(token: Token[], vault: string) {
         try {
             if (token.length < 2) return;
 
-            const balance = await vaultBalance(vault);
+            let balance: { [key: string]: number };
+            if (!tokenBalance) {
+                balance = await vaultBalance(vault);
+                setTokenBalance(balance);
+            } else balance = tokenBalance;
+
             if (balance[_token.address] <= 0) return;
 
-            const parsedAmount = parseToBigNumber(amount, _token.decimals);
+            const ratio = amount / balance[_token.address];
 
-            // **** Set the new token balance at the end
+            setTokenAmount((tknAmnt) => {
+                for (const tkn of token) if (balance[tkn.address] > 0) tknAmnt[tkn.address] = balance[tkn.address] * ratio;
+
+                return tknAmnt;
+            });
         } catch (e: any) {}
-
-        // setTokenAmount((tknAmnt) => {
-        //     tknAmnt[token.address] = amount;
-        //     if (quote) for (const [address, amount] of Object.entries(quote)) if (amount) tknAmnt[parseAddress(address)] = amount;
-
-        //     return { ...tknAmnt };
-        // });
     }
 
     return { tokenAmount, setTokenAmount: _setTokenAmount };
