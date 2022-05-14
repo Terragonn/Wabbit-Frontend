f25d9eb926fe434a853465ff7db1d84c8aea5b62
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-05-15 07:53:44 +1000

    🎉 Update

--- src/head/BaseHead.tsx
+++ src/head/BaseHead.tsx
@@ -2,20 +2,39 @@ import Head from "next/head";
 import Script from "next/script";
 
 export default function BaseHead() {
+    const title = "Torque Money - DeFi The Norm";
+    const description = "A decentralized on-chain aggregator of yield aggregators platform powered by the Fantom blockchain.";
+    const url = "https://app.torque.money";
+    const bannerUrl = "https://i.imgur.com/voyXWCK.png";
+    const twitterHandle = "@torque_money";
+
     return (
         <>
             <Head>
+                {/* Main section */}
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="manifest" href="/manifest.json" />
-
-                <title>Torque Money - DeFi The Norm</title>
-                <meta name="description" content="A decentralized on-chain aggregator of yield aggregators platform powered by the Fantom blockchain." />
-
                 <meta name="theme-color" content="#921ebb" />
                 <meta charSet="UTF-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="robots" content="index, follow" />
-                <link rel="canonical" href="https://app.torque.money" />
+                <link rel="canonical" href={url} />
+
+                <title>{title}</title>
+                <meta name="description" content={description} />
+
+                {/* OG Section */}
+                <meta name="og:title" content={title} />
+                <meta name="og:description" content={description} />
+                <meta name="og:image" content={bannerUrl} />
+                <meta property="og:url" content={url} />
+
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content={title} />
+                <meta name="twitter:description" content={description} />
+                <meta property="twitter:url" content={url} />
+                <meta name="twitter:image" content={bannerUrl} />
+                <meta name="twitter:site" content={twitterHandle} />
             </Head>
             <div>
                 <Script src="https://www.googletagmanager.com/gtag/js?id=G-M831HRNWYT" strategy="afterInteractive" />
